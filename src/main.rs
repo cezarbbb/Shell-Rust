5dcc5ac443f326cae40236e9e8be74a7cde617ec
cezarbbb <btq19991016@gmail.com>
2024-06-01 21:45:30 +0900

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,7 @@ fn main() {
     let mut input = String::new();
     match stdin.read_line(&mut input) {
         Ok(n) => {
-            println!("{n} bytes read");
-            println!("{input}");
+            println!("{input}: command not found");
         }
         Err(error) => println!("error: {error}"),
     }
