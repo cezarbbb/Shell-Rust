4c326e7581fcf0bc6b54626f23b56c91abe6793b
cezarbbb <btq19991016@gmail.com>
2024-06-01 22:09:25 +0900

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,13 @@ fn main() {
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
-    match stdin.read_line(&mut input) {
+    loop {
+        match stdin.read_line(&mut input) {
         Ok(n) => {
             let output = &input[..n-1];
             print!("{output}: command not found\n");
         }
         Err(error) => println!("error: {error}"),
     }
-
+    }
 }
