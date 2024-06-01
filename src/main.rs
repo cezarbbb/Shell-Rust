88752f549e5f4af8c7a60a24d7f69ece6d801704
cezarbbb <btq19991016@gmail.com>
2024-06-01 21:05:53 +0900

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@
 use std::io::{self, Write};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     print!("$ ");
     io::stdout().flush().unwrap();
 
