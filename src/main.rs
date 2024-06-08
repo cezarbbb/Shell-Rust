88cd0ba30dba254d279c442f13287e74aabacc44
cezarbbb <btq19991016@gmail.com>
2024-06-08 23:02:51 +0900

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io::{self, Write};
 fn main() {
     // Wait for user input
     let stdin = io::stdin();
+    let path_env = std::env::var("PATH").unwrap();
     
     loop {
         let mut input = String::new();
@@ -15,9 +16,15 @@ fn main() {
             ["exit", _code] => break,
             ["echo", ..] => print!("{}", args[1..].join(" ")),
             ["type", arg, ..] => {
-                match arg.trim() {
-                    "exit" | "echo" | "type" => print!("{} is a shell builtin\n", arg.trim()),
-                    _ => print!("{} not found\n", arg.trim()),
+                // match arg.trim() {
+                //     "exit" | "echo" | "type" => print!("{} is a shell builtin\n", arg.trim()),
+                //     _ => print!("{} not found\n", arg.trim()),
+                // }
+                let mut path_split = path_env.split(':');
+                if let Some(path) = path_split.find(|path| std::fs::metadata(format!("{}/{}", path, arg)).is_ok()) {
+                    print!("{arg} is {path}/{arg}");
+                } else {
+                    print!("missing_cmd: {arg} not found");
                 }
             }
             _ => print!("{}: command not found\n", input.trim()),
